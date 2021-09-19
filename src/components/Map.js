dd75ace2cb04acab4090283ad4dbe8605168d653
Gonu Kim <gonukim@MacBook-Air.hitronhub.home>
2021-09-18 19:38:05 -0700

    added marker for bus stops

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,11 @@
 // import { Map, GoogleApiWrapper } from 'google-maps-react';
-import React, {Component} from 'react';
+import React, {Component, useEffect} from 'react';
 import dark from './GoogleMapStyles';
 import {GoogleMap, useLoadScript, Marker, InfoWindow} from '@react-google-maps/api';
 import usePlacesAutocomplete, {getGeoCode, getLatLng} from "use-places-autocomplete";
+import axios from "axios";
 
+const translinkAPI = 'uR1LJ7QcIfeLZmaQ0oPs';
 
 const libraries = ["places"]
 const mapContainerStyle = {
@@ -14,11 +16,18 @@ const center = {
     lat: 49.2827,
     lng: -123.1207
 }
+
+let currentCenter = center;
+
 const options = {
     styles: dark,
     disableDefaultUI: true,
     zoomControl: true,
 }
+
+let animateTrigger = 1;
+
+let ref;
 
 export default function Map({value}) {
     const { isLoaded, loadError } = useLoadScript({
@@ -26,44 +35,97 @@ export default function Map({value}) {
         libraries,
     })
 
-    const [currentLocationMarker, setCurrentLocationMarker] = React.useState({
-        lat: '',
-        lng: '',
-        time: ''
-    });
-    const [markers, setMarkers] = React.useState([]);
-
+    // Map Reference
     const mapRef = React.useRef();
     const onMapLoad = React.useCallback((map) => {
         mapRef.current = map;
     }, []);
 
+    // Pan to set location
     const panTo = React.useCallback(({lat, lng}) => {
         mapRef.current.panTo({lat, lng});
         mapRef.current.setZoom(14);
+        setNewPos({lat, lng});
     }, []);
 
+    const [currentLocationMarker, setCurrentLocationMarker] = React.useState({
+        lat: '',
+        lng: '',
+        time: ''
+    });
+
+    const [currentPositon, setCurrentPosition] = React.useState();
+
+    const [data, setData] = React.useState([]);
+    
+    const [stopMarkers, setStopMarkers] = React.useState([]);
+
+    const [newPos, setNewPos] = React.useState();
+
+    const [clickedLocation, setClickedLocation] = React.useState(false);
+
+    const fetchApiData = () => {
+        if (newPos !== undefined) {
+            // console.log(newPos)
+            let lat = newPos.lat.toString();
+            let lng = newPos.lng.toString();
+            lat = lat.slice(0, (lat.indexOf(".")) + 7);
+            lng = lng.slice(0, (lng.indexOf(".")) + 7);
+
+            axios.get("https://api.translink.ca/rttiapi/v1/stops?apikey=uR1LJ7QcIfeLZmaQ0oPs&lat=" + lat + "&long=" + lng + "&radius=2000")
+            .then((response) => setData(response.data));
+        } 
+    }
+
+    // useEffect(() => {
+    //     fetchApiData();
+    // }, []);
+    
+      function handleCenterChanged() {
+        if (!mapRef.current) return;
+
+        const position = mapRef.current.getCenter().toJSON();
+        setNewPos(position);
+        
+        
+        // console.log(newPos);
+        fetchApiData();
+    }
 
     if(loadError) return "Error loading maps";
     if(!isLoaded) return "Loading Maps";
     
     return (
         <div style={value !== 0 ? {display: "none"} : {display: "block"}}>
-            {currentLocation(panTo, setCurrentLocationMarker)}
+            {currentLocation(panTo, setCurrentLocationMarker, handleCenterChanged)}
             <GoogleMap 
+                onCenterChanged={handleCenterChanged}
                 mapContainerStyle={mapContainerStyle}
-                zoom={8}
+                zoom={11}
                 center={center}
                 options={options}
                 onClick={() => {}}
-                onLoad={onMapLoad}>
-                     <Marker 
-                            position={{lat: currentLocationMarker.lat, lng: currentLocationMarker.lng}} 
+                onLoad={onMapLoad}
+            >
+                {markCurrentLocation(currentLocationMarker)}
+                {
+                    data.map((stop) => {
+                        // console.log(stop);
+                        return <Marker 
+                            key={stop.StopNo}
+                            position={{lat: stop.Latitude, lng: stop.Longitude}}
                             icon={{
-                                url: '/blue-dot.png',
-                                scaledSize: new window.google.maps.Size(25, 25),
+                                url: '/bus-stop2.png',
+                                scaledSize: new window.google.maps.Size(45, 45),
                             }}
-                    />
+                        />
+                    })
+                }
+                {/* {placeBusStopMarkers(newPos, setData, data, moved, setMoved)} */}
+                
+                    {
+                        // placeBusStopMarkers(panTo, setStopMarkers)
+                    }
                     {/* {currentLocationMarker(marker => 
                         <Marker 
                             key={marker.time.toISOString()} 
@@ -79,11 +141,38 @@ export default function Map({value}) {
     );
 }
 
-function currentLocation(panTo, setCurrentLocationMarker) {
+// function placeBusStopMarkers(newPos, setData, data, moved, setMoved) {
+//     if (newPos != null && moved) {
+//         console.log(newPos);
+//         axios.get(`https://api.translink.ca/rttiapi/v1/stops?apikey=uR1LJ7QcIfeLZmaQ0oPs&lat=49.2827&long=-123.1207&radius=500`)
+//             .then((response) => setData(response.data));
+//         console.log(data);
+//         setMoved(false)
+//         return (
+//             <div></div>
+//         )
+//     }
+// }
+
+function markCurrentLocation(currentLocationMarker) {
+    if (currentLocationMarker.lat !== '') {
+        return (
+            <Marker 
+            position={{lat: currentLocationMarker.lat, lng: currentLocationMarker.lng}} 
+            icon={{
+                url: '/blue-dot.png',
+                scaledSize: new window.google.maps.Size(25, 25),
+            }}
+        />
+        )
+    }
+}
+
+function currentLocation(panTo, setCurrentLocationMarker, handleCenterChanged) {
     return (
         <button className="locate" onClick={() => {
+            
             navigator.geolocation.getCurrentPosition((position) => {
-                console.log(position)
                 panTo({
                     lat: position.coords.latitude,
                     lng: position.coords.longitude,
@@ -94,11 +183,18 @@ function currentLocation(panTo, setCurrentLocationMarker) {
                     time: new Date(),
                 })
             }, () => null);
+            handleCenterChanged();
         }}>
             <img src="locate.svg" alt="locate me icon"/>
         </button>
     );
 }
+
+// function loadAllBusStops() {
+//     fetch('https://api.translink.ca/rttiapi/v1/stops?apikey=uR1LJ7QcIfeLZmaQ0oPs&lat=49.248523&long=-123.108800&radius=2000')
+//         .then(response => response.json())
+//         .then(data => console.log(data));
+// }
 
 
 
