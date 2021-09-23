27c6f600568ec44b0325eb9ccb534e4360d2838e
Gonu Kim <gonukim@MacBook-Air.hitronhub.home>
2021-09-23 16:32:58 -0700

    edited contact us page

--- src/components/layout/Tab.js
+++ src/components/layout/Tab.js
@@ -8,7 +8,7 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import MapContainer from '../Map';
 import './Tab.css';
-import ContactUs from '../ContactUs';
+import ContactForm from '../ContactUs';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -71,7 +71,7 @@ export default function SimpleTabs() {
       </TabPanel>
       <MapContainer value={value} />
       <TabPanel value={value} index={1}>
-        <ContactUs />
+        <ContactForm />
       </TabPanel>
     </div>
   );
